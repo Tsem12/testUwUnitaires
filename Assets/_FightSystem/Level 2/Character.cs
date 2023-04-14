99b34aa9cb0b7a4f1096157514996ccb59fd49a9
Orcoro <45428006+Orcoro@users.noreply.github.com>
2023-04-14 23:09:09 +0200

    Update Character.cs

--- Assets/_FightSystem/Level 2/Character.cs
+++ Assets/_FightSystem/Level 2/Character.cs
@@ -110,8 +110,10 @@ namespace _2023_GC_A2_Partiel_POO.Level_2
             modifier = TypeResolver.GetFactor(attacker.BaseType, this.BaseType) * TypeResolver.GetSTAB(attacker.BaseType, attacker.CurrentAttack.Type);
             damage = Mathf.FloorToInt(((attacker.CurrentAttack.Power * (attacker.Attack / this.Defense)) / 50) * modifier);
             CurrentHealth -= damage;
-            if (attacker.CurrentAttack.Status != StatusPotential.NONE)
+            if (attacker.CurrentAttack.Status != StatusPotential.NONE) {
                 CurrentStatus = StatusEffect.GetNewStatusEffect(attacker.CurrentAttack.Status);
+                Debug.Log("Status : " + CurrentStatus.Status);
+            }
             Debug.Log("Damage : " + damage + " Health : " + CurrentHealth);
             if (CurrentHealth < 0)
                 CurrentHealth = 0;
