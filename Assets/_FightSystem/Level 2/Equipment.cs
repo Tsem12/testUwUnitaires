3f51356246270ebdbe4a3e5c5b92e88df96326bd
Tsem12 <82233249+Tsem12@users.noreply.github.com>
2023-04-14 17:38:27 +0200

    start creating test + add equipement type

--- Assets/_FightSystem/Level 2/Equipment.cs
+++ Assets/_FightSystem/Level 2/Equipment.cs
@@ -6,19 +6,28 @@ namespace _2023_GC_A2_Partiel_POO.Level_2
     /// </summary>
     public class Equipment
     {
+        public enum EquipementType
+        {
+            None,
+            AttackPriority
+        };
+
         private int _bonusHealth;
         private int _bonusAttack;
         private int _bonusDefense;
         private int _bonusSpeed;
+        private EquipementType _equipement;
 
-        public Equipment(int bonusHealth, int bonusAttack, int bonusDefense, int bonusSpeed)
+        public Equipment(int bonusHealth, int bonusAttack, int bonusDefense, int bonusSpeed, EquipementType equipement)
         {
             BonusAttack = bonusAttack;
             BonusDefense = bonusDefense;
             BonusSpeed = bonusSpeed;
             BonusHealth = bonusHealth;
+            Equipement = equipement;
         }
 
+        public EquipementType Equipement { get => _equipement; protected set => _equipement = value; }
         public int BonusHealth { get => _bonusHealth; protected set => _bonusHealth = value; }
         public int BonusAttack { get => _bonusAttack; protected set => _bonusAttack = value; }
         public int BonusDefense { get => _bonusDefense; protected set => _bonusDefense = value; }
