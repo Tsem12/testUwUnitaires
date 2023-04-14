2c7ac76f1d245345cb013180a5ce1dc26873c4d9
Tsem12 <82233249+Tsem12@users.noreply.github.com>
2023-04-14 16:38:45 +0200

    equipement

--- Assets/_FightSystem/Level 2/Equipment.cs
+++ Assets/_FightSystem/Level 2/Equipment.cs
@@ -6,16 +6,26 @@ namespace _2023_GC_A2_Partiel_POO.Level_2
     /// </summary>
     public class Equipment
     {
+<<<<<<< Updated upstream
         
+=======
+        private int _bonusHealth;
+        private int _bonusAttack;
+        private int _bonusDefense;
+        private int _bonusSpeed;
+
+>>>>>>> Stashed changes
         public Equipment(int bonusHealth, int bonusAttack, int bonusDefense, int bonusSpeed)
         {
-            
+            BonusAttack = bonusAttack;
+            BonusDefense = bonusDefense;
+            BonusSpeed = bonusSpeed;
+            BonusHealth = bonusHealth;
         }
 
-        public int BonusHealth { get; protected set; }
-        public int BonusAttack { get; protected set; }
-        public int BonusDefense { get; protected set; }
-        public int BonusSpeed { get; protected set; }
-
+        public int BonusHealth { get => _bonusHealth; protected set => _bonusHealth = value; }
+        public int BonusAttack { get => _bonusAttack; protected set => _bonusAttack = value; }
+        public int BonusDefense { get => _bonusDefense; protected set => _bonusDefense = value; }
+        public int BonusSpeed { get => _bonusSpeed; protected set => _bonusSpeed = value; }
     }
 }
